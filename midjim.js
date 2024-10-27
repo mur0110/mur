b6d9699c7c96d3f3cfc1fe4520207ef1947186a7
mur0110 <aqxiy@proton.me>
2024-10-26 20:45:41 -0500

    Create midjim.js

---
+++ midjim.js
@@ -0,0 +1,109 @@
+const interDuration = .15;
+
+const songPath = 'midjim.mp3';
+
+
+const volArr=[3, 5, 7];
+const speedArr=[3, 5, 7];
+
+const multVolArr = [.445, .46, .465, .475, .5, .525, .535, .54, .555];
+const multSpeedArr = [.89, .92 ,.93, .95, 1, 1.05, 1.07, 1.08, 1.11];
+
+
+const randomsForVol=[];
+const randomsForSpeed=[];
+
+let randomsForVolIter = 0;
+let randomsForSpeedIter = 0;
+
+for (let i=0; i<600; i++){
+    randomsForVol.push(pickAmongNine());
+    randomsForSpeed.push(pickAmongNine());
+}
+
+let presVolIntervalIter = 0;
+let presSpeedIntervalIter = 0;
+
+let presVolInterval = 0;
+let presSpeedInterval = 0;
+
+function redefineVolInterval(){
+    presVolInterval = volArr[presVolIntervalIter];
+}
+
+function redefineSpeedInterval(){
+    presSpeedInterval = speedArr[presSpeedIntervalIter];
+}
+
+function pickAmongNine(){
+    return Math.floor(Math.random() * 9);
+}
+
+function nextVolIntervalIter(){
+    if (presVolIntervalIter < 2){
+        presVolIntervalIter++;
+    }
+    else{
+        presVolIntervalIter=0;
+    }
+}
+
+function nextSpeedIntervalIter(){
+    if (presSpeedIntervalIter < 2){
+        presSpeedIntervalIter++;
+    }
+    else{
+        presSpeedIntervalIter=0;
+    }
+}
+
+
+
+function runner(){
+    const songAudio = new Audio(songPath);
+    
+    let randomForVolVal = multVolArr[randomsForVol[randomsForVolIter]];
+    let randomForSpeedVal = multSpeedArr[randomsForSpeed[randomsForSpeedIter]];
+
+
+
+    let volIntervalIter = 0;
+    let speedIntervalIter = 0;
+    redefineSpeedInterval();
+    redefineVolInterval();
+
+
+    let volInterval = presVolInterval;
+    let speedInterval = presSpeedInterval;
+
+
+    songAudio.play();
+    advanceVolume();
+    advanceSpeed();
+
+    function advanceVolume(){
+          setTimeout(function(){
+              songAudio.volume = randomForVolVal;
+              volIntervalIter += volArr[presVolIntervalIter];
+              advanceVolume();
+          }, interDuration * volInterval * 1000);
+          nextVolIntervalIter();
+          redefineVolInterval();
+          volInterval = presVolInterval;
+          randomForVolVal = multVolArr[randomsForVol[randomsForVolIter]];
+          randomsForVolIter++;
+    }
+
+    function advanceSpeed(){
+          setTimeout(function(){
+              songAudio.playbackRate = randomForSpeedVal;
+              speedIntervalIter += speedArr[presSpeedIntervalIter];
+              advanceSpeed();
+          }, interDuration * speedInterval * 1000);
+          nextSpeedIntervalIter();
+          redefineSpeedInterval();
+          speedInterval = presSpeedInterval;
+          randomForSpeedVal = multSpeedArr[randomsForSpeed[randomsForSpeedIter]];
+          randomsForSpeedIter++;
+    }
+}
